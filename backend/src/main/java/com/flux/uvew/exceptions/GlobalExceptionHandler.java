2ef17c0573a0a395cdcc9acb1f9bd932e8ab5fdb
Gaurav Dubey <gd13471944@gmail.com>
2023-11-28 12:13:34 +0530

    cleanup changes

--- backend/src/main/java/com/flux/uvew/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/flux/uvew/exceptions/GlobalExceptionHandler.java
@@ -1,19 +1,19 @@
 package com.flux.uvew.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(
             GlobalExceptionHandler.class);
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception ex) {
-        // You can customize the error response here
+        LOGGER.error(ex.getMessage(), ex);
         String errorMessage = "An error occurred: " + ex.getMessage();
         return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
     }
