f1ab61c6253ec44fd29cce26d43a744844e02943
Gaurav Dubey <gd13471944@gmail.com>
2024-01-02 16:13:29 +0530

    Revert "save keycloak user to db"
    This reverts commit ec85e3a04065894cfa624b0237e40da22b958cb9.

--- frontend/uvew_ui/src/app/app-routing.module.ts
+++ frontend/uvew_ui/src/app/app-routing.module.ts
@@ -7,15 +7,16 @@ import { VideoDetailComponent } from './components/home/video-detail/video-detai
 import { UploadVideoComponent } from './components/upload/upload-video/upload-video.component';
 import { AccessDeniedComponent } from './access-denied/access-denied.component';
 import { AuthGuard } from './auth.guard';
-import { CallbackComponent } from './components/util/callback/callback.component';
 const routes: Routes = [
-  { path: 'callback', component: CallbackComponent},
   { path: 'dashboard', component: DashboardComponent ,canActivate: [AuthGuard]},
   { path: 'upload-video', component: UploadVideoComponent ,canActivate: [AuthGuard]},
   { path: 'save-video-detail', component: SaveVideoDetailComponent ,canActivate: [AuthGuard]},
   { path: 'video-details/:videoId', component: VideoDetailComponent,canActivate: [AuthGuard] },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
-  { path: 'access-denied',component: AccessDeniedComponent,},
+  {
+    path: 'access-denied',
+    component: AccessDeniedComponent,
+  },// redirect to `first-component`
   { path: '**', component: PagenotfoundComponent }, // Wildcard route for a 404 page
   
 ];
