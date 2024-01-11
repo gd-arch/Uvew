069d38e24d24e26a796528ebbff9e3cc011c05b4
Gaurav Dubey <gd13471944@gmail.com>
2024-01-11 17:34:51 +0530

    updated cors prop to frontend address

--- backend/src/main/java/com/flux/uvew/controller/UserController.java
+++ backend/src/main/java/com/flux/uvew/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 public class UserController {
     private final UserService userService;
